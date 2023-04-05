203f3a19ad3942ea5a56b7c1735ee13564dca0d3
IsadinovDias312 <112857129+IsadinovDias312@users.noreply.github.com>
2023-04-05 19:57:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,16 +13,15 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     time_92 = 92
     alpha = 1 - p
-    semians = 0
-    for i in range(len(x)):
-      semians += x[i]
-    semians /= len(x)
-    left = semians - scipy.stats.gamma.ppf((1+alpha)/2, 1)/len(x)
-    right = semians - scipy.stats.gamma.ppf((1-alpha)/2, 1)/len(x)
-    left += 1/2
-    right += 1/2
-    left /= time_92*time_92
-    right /= time_92*time_92
+    semians = max(x)
+    left = semians - math.log(alpha)/len(x)
+    right = semians - math.log(1-alpha)/len(x)
+    left -= 1/2
+    right -= 1/2
+    left /= (time_92*time_92)
+    right /= (time_92*time_92)
+    left *= 2
+    right *= 2
 
     return left, \
            right

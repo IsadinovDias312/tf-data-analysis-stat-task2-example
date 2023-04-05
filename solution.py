f7ccf866c36033c0b4fc4006f2b104c780944896
IsadinovDias312 <112857129+IsadinovDias312@users.noreply.github.com>
2023-04-05 15:07:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import numpy as np
+import math
 
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1182463770 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    time_92 = 92
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    loc = min(x)
+    loc1 = loc - (math.log(alpha)/len(x))+1/2
+    loc2 = loc + 1/2
+    loc1 /= time_92*time_92
+    loc2 /= time_92^2*time_92
+    return loc1, \
+           loc2

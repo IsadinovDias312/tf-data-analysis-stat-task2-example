e12b860a670ad38be67da6259e032cbdf36fd10b
IsadinovDias312 <112857129+IsadinovDias312@users.noreply.github.com>
2023-04-06 22:44:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,19 +8,19 @@ import scipy.stats
 chat_id = 1182463770 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     time_92 = 92
     alpha = 1 - p
     semians = 0
-    for i in range(len(x)):
-      semians += x[i]
-    semians /= len(x)
-    left = semians - 1/2 - scipy.stats.gamma.ppf((1+alpha)/2, len(x))/len(x)
-    right = semians - 1/2 - scipy.stats.gamma.ppf((1-alpha)/2, len(x))/len(x)
-    left /= (time_92*time_92)
-    right /= (time_92*time_92)
+    for i in range(len(x)): #x_i ~ teta - exp(1)
+      semians += x[i] #teta = (a*t^2)/2 + 1/2
+    semians /= len(x) #G(x,teta)= -(sum(x_i)-n*teta) ~ Gamma(1,n)
+    # (u_alpha/2)/n + x.mean < teta < u_(1-alpha/2)/n + x.mean 
+    left = semians + scipy.stats.gamma.ppf((alpha)/2, len(x))/len(x) 
+    right = semians + scipy.stats.gamma.ppf(1-alpha/2, len(x))/len(x)
+    left -= 1/2 # express a
+    right -= 1/2
+    left /= (time_92**2)
+    right /= (time_92**2)
     left *= 2
     right *= 2
 
